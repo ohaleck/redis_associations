e7ad99d5a52e707df9b7c9f3e4863068a57f4072
Maciek O <ohaleck@gmail.com>
2011-08-10 21:57:04 +0200

    Fixed

--- lib/redis_associations/redis_associations.rb
+++ lib/redis_associations/redis_associations.rb
@@ -6,119 +6,92 @@ module RedisAssociations
   end
 
   module ClassMethods
+
+    def has_many_through_redis(associations, options={})
+      redis_association(associations, true, options)
+    end
+
     def has_one_through_redis(association, options={})
-      # association = :model or :pkg/model'
+      redis_association(association, false, options)
+    end
+
+
+    protected
+
+    # association = :models or :'pkg/models' for many
+    # association = :model or :'pkg/model' for one
+    def redis_association(association, many, options={})
       # full_association_class_name = 'Model' or 'Pkg::Model'
       full_association_class_name = association.to_s.singularize.camelize
       # association_class_name = 'Model'
       association_class_name = full_association_class_name.demodulize
-      # association_name = :model
-      association_name = (options[:as] || association_class_name.underscore).to_sym
-      # association_id = :model_id
-      association_id = association_class_name.foreign_key.to_sym
-      # association_key = :models_key
-      association_key = "#{association_name.to_s.pluralize}_key".to_sym
+      # attribute = :model or :models
+      attribute = options[:as] || association_class_name
+      attribute = (many ? attribute.pluralize : attribute).underscore.to_sym
+      # id_attribute = :model_id or :model_ids
+      id_attribute = association_class_name.foreign_key
+      id_attribute = (many ? id_attribute.pluralize : id_attribute).to_sym
+      # association_key_method = :models_key
+      association_key_method = "#{attribute.to_s.pluralize}_key".to_sym # always plural, regardless of the value of many
 
-      #  def models
-      define_method(association_name) do
-        id = self.send(association_id)
-        a = association_class_name.constantize.find(id)
-        a.is_a?(Array) ? a.first : a
+      # def model or models
+      define_method(attribute) do
+        ids = self.send(id_attribute)
+        objects = full_association_class_name.constantize.find(ids)
       end
 
-      # def model_ids
-      define_method(association_id) do
-        key = self.send(association_key)
-        redis.get(key).to_i
-      end
-
-      # def models=
-      define_method(:"#{association_name}=") do |object|
-        # TODO allow id attribute other than "id""
-        # TODO check object class and existence
-        self.send(:"#{association_id}=", object.id)
-      end
-
-      # def model_ids=
-      define_method(:"#{association_id}=") do |_id|
-        key = self.send(association_key)
-        redis.set key, _id
-      end
-
-      # def models_key
-      define_method(association_key) do
-        # e.g.: ra:post:1234:category
-        self.class.send(association_key, self.id)
-      end
-
-      selv = self
-      self.class.instance_eval do
-        # def self.models_key
-        define_method(association_key) do |_id|
-          raise "id must be given" unless _id
-          "#{RA_PREFIX}:#{selv.to_s.downcase}:#{_id}:#{association_id}"
+      # def model_id or model_ids
+      define_method(id_attribute) do
+        key = self.send(association_key_method)
+        ids = redis.smembers(key).map(&:to_i)
+        if many
+          ids
+        else
+          ids.first
         end
       end
-    end
-
-    def has_many_through_redis(associations, options={})
-      # associations = :models or :'pkg/models'
-      # full_association_class_name = 'Model' or 'Pkg::Model'
-      full_associations_class_name = associations.to_s.singularize.camelize
-      # association_class_name = 'Model'
-      associations_class_name = full_associations_class_name.demodulize
-      # association_name = :models
-      associations_name = (options[:as] || associations_class_name.pluralize.underscore).to_sym
-      # association_ids = :model_ids
-      association_ids = associations_class_name.foreign_key.pluralize.to_sym
-      # associations_key = :models_key
-      associations_key = "#{associations_name}_key".to_sym
-      
-      # def models
-      define_method(associations_name) do
-        ids = self.send(association_ids)
-        full_associations_class_name.constantize.find(ids)
-      end
-
-      # def model_ids
-      define_method(association_ids) do
-        key = self.send(associations_key)
-        redis.smembers(key).map(&:to_i)
-      end
 
       # def models=
-      define_method(:"#{associations_name}=") do |objects|
+      define_method(:"#{attribute}=") do |objects|
         # TODO allow id attribute other than "id""
-        ids = objects.map(&:id)
-        self.send(:"#{association_ids}=", ids)
+        ids = if many
+          objects.map(&:id)
+              else
+          objects.id
+        end
+        self.send(:"#{id_attribute}=", ids)
       end
 
       # def model_ids=
-      define_method(:"#{association_ids}=") do |ids|
-        key = self.send(associations_key)
-        redis.sdiffstore key, key, key
-        ids.each do |oid|
+      define_method(:"#{id_attribute}=") do |ids|
+        key = self.send(association_key_method)
+        redis.del key
+        [ids].flatten.each do |oid|
           redis.sadd key, oid
         end
       end
 
       # def models_key
-      define_method(associations_key) do
+      define_method(association_key_method) do
         # e.g.: ra:post:1234:comments
-        self.class.send(associations_key, self.id)
+        raise "id must be set" unless self.id
+        self.class.send(association_key_method, self.id, options)
       end
 
-      selv = self
+      elv = self
       self.class.instance_eval do
         # def self.models_key
-        define_method(associations_key) do |_id|
+        define_method(association_key_method) do |_id, *options|
+          options ||= {}
           raise "id must be given" unless _id
-          "#{RA_PREFIX}:#{selv.to_s.downcase}:#{_id}:#{association_ids}"
+          if options.delete(:keep_package_name)
+            "#{RA_PREFIX}:#{self.to_s.underscore}:#{_id}:#{id_attribute}"
+          else
+            "#{RA_PREFIX}:#{self.to_s.demodulize.underscore}:#{_id}:#{id_attribute}"
+          end
         end
       end
     end
-
-    protected
   end
 
   def redis
